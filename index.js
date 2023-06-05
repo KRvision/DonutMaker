b5be62196264135524d3d84f38b07c18c77405f4
krvision <ray.samuel907@gmail.com>
2023-06-05 19:34:40 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
-const container = (document.querySelector('.container').innerText =
-  'mmmmmmmmm doughnuts.... ahhhhhhh!');
+//ITERATION 1
+
+let donutCount = 0;
+
+//Get our button element
+const btn = document.querySelector("button");
+
+function addToDonutCount(amountToAdd) {
+  donutCount = donutCount + amountToAdd;
+}
+
+//Retrieve donut count
+function getDonutCount() {
+  return donutCount;
+}
+
+// Purchase Auto Clicks
+let autoClickCount = 0;
+
+function addToAutoClickCount(amountToAdd) {
+  autoClickCount = autoClickCount + amountToAdd;
+}
+
+//Retrieve autoClick count
+function addToAutoClickCount() {
+  return autoClickCount;
+}
+function subtractFromDonutCount(amountToSubtract) {
+  donutCount = donutCount - amountToSubtract;
+}
+
+//Cost of second Auto Clicker increase
+let autoClickCost = 100;
+
+function autoClickCostIncrease() {
+  autoClickCost = autoClickCost * 1.1;
+}
+
+//Add a click event listener to our donut button
+btn.addEventListener(
+  "click",
+  function () {
+    donutCount++;
+    document.getElementById("nav-menu").innerText = donutCount;
+  },
+  1000
+);
