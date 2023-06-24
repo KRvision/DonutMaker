838007dd6e75cd130bed5cc5ede68efaf7e1faa1
krvision <ray.samuel907@gmail.com>
2023-06-24 06:14:20 -0400

    Added modal content

--- index.js
+++ index.js
@@ -103,3 +103,29 @@ function reset() {
   document.getElementById("click-btn").innerText = donutCount;
   document.getElementById("remaining-clicks").innerText = autoClickerCost;
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var popup = document.getElementById("modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+popup.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
